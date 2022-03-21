92e908fd3d39699ce060167c5ea4a7b5fb385244
ngocthanh-shu <ngocthanh.shu@gmail.com>
2022-03-21 21:08:36 +0700

    update controller and route

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes/index.route');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded(
@@ -23,22 +25,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/',(req,res)=>{
-    res.render('home');
-});
-
-app.get('/news', (req, res) => {
-    res.render('news');
-});
-
-app.get('/search', (req, res) => {
-    res.render('search');
-});
-
-app.post('/search', (req, res) => {
-    console.log(req.body);
-    res.send();
-});
+//route init
+route(app);
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port} at http://localhost:${port}`);
