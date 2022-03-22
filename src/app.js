321cf38658f1a39ac9fb568142a33f79164e0f43
ngocthanh-shu <ngocthanh.shu@gmail.com>
2022-03-22 15:05:41 +0700

    install prettier lint-staged husky

--- src/app.js
+++ src/app.js
@@ -8,26 +8,31 @@ const port = 3000;
 
 const route = require('./routes/index.route');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(express.urlencoded(
-    {
-        extended: true
-    }
-));
-app.use(express.json());
+        app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+    app.use(express.json());
 
 app.use(morgan('dev'));
 
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
-}));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: 'hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 //route init
 route(app);
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port} at http://localhost:${port}`);
-}); 
+app.listen(port, () => {
+    console.log(
+        `Server is running on port ${port} at http://localhost:${port}`,
+    );
+});
