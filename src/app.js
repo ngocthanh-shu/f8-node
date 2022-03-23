a1ce60e8d60a46d2afda7a35bb86ef5d43b9ab1e
ngocthanh-shu <ngocthanh.shu@gmail.com>
2022-03-23 14:21:00 +0700

    CRUD show index view

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 //route init
 route(app);
--- src/app/controllers/site.controller.js
+++ src/app/controllers/site.controller.js
@@ -1,13 +1,25 @@
 const Course = require('../models/course.model');
+const { multipleMongooseToObject } = require('../../util/mongoose.util');
 
 class SiteController {
     //[GET] /home
-    index(req, res) {
+    index(req, res, next) {
         // res.render('home');
-        Course.find({}, function (err, courses) {
-            if (!err) res.json(courses);
-            else res.status(400).json({ error: 'error' });
-        });
+        //callback
+        // Course.find({}, function (err, courses) {
+        //     if (!err) res.json(courses);
+        //     else next(err);
+
+        // });
+
+        //promise
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
     }
 
     //[GET] /search
