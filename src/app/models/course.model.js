a5ec9f11b782ad608f549060a8f845b7690d420a
ngocthanh-shu <ngocthanh.shu@gmail.com>
2022-03-24 14:48:57 +0700

    create a  course

--- src/app/models/course.model.js
+++ src/app/models/course.model.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const Course = new Schema({
-    name: { type: String, maxlength: 255 },
-    desc: { type: String, maxlength: 600 },
-    img: { type: String, maxlength: 255 },
-    slug: { type: String, maxlength: 255 },
-    videoId: { type: String, maxlength: 255 },
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
-});
+const Course = new Schema(
+    {
+        name: { type: String, maxlength: 255 },
+        desc: { type: String, maxlength: 600 },
+        img: { type: String, maxlength: 255 },
+        slug: { type: String, slug: 'name', unique: true },
+        videoId: { type: String, maxlength: 255 },
+    },
+    {
+        timestamps: true,
+    },
+);
 
 module.exports = mongoose.model('Course', Course);
--- src/routes/courses.route.js
+++ src/routes/courses.route.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const coursesController = require('../app/controllers/course.controller');
 
 // newsController.index
+
+router.get('/create', coursesController.create);
+router.post('/store', coursesController.store);
 router.get('/:slug', coursesController.show);
 
 module.exports = router;
